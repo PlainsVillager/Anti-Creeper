835ac2d82b5f641baa92d8b6236817dcea809223
PlainsVillager <littlecuckoo@outlook.com>
2023-02-04 08:11:37 +0800

    first commit

---
+++ src/main/java/io/github/plainsvillager/anticreeper/AntiCreeper.java
@@ -0,0 +1,12 @@
+package io.github.plainsvillager.anticreeper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AntiCreeper implements ModInitializer {
+
+    public static final String MOD_ID = "anticreeper";
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/plainsvillager/anticreeper/mixin/CreeperEntityMixin.java
@@ -0,0 +1,27 @@
+package io.github.plainsvillager.anticreeper.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(CreeperEntity.class)
+public abstract class CreeperEntityMixin extends LivingEntity {
+    public CreeperEntityMixin(EntityType<? extends CreeperEntity> entityType, World world) {
+        super(entityType, world);
+    }
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void explode() {
+        if (!this.world.isClient) {
+            this.dead = true;
+            this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0F, World.ExplosionSourceType.NONE);
+            this.discard();
+        }
+    }
+}
