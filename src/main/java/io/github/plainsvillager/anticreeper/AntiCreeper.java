f167fdc2e8af6976498540e9a045386f91264a54
PlainsVillager <littlecuckoo@outlook.com>
2023-02-05 14:57:20 +0800

    BIG: 1.4-BETA.1: ADD GAMERULE TO CONFIG THE EXPLOSION OF CREEPER!

--- src/main/java/io/github/plainsvillager/anticreeper/AntiCreeper.java
+++ src/main/java/io/github/plainsvillager/anticreeper/AntiCreeper.java
@@ -1,13 +1,30 @@
 package io.github.plainsvillager.anticreeper;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 
 public class AntiCreeper implements ModInitializer {
 
     public static final String MOD_ID = "anticreeper";
+    public static World world;
+    public static final GameRules.Key<GameRules.BooleanRule> DO_CREEPERS_EXPLOSION_DESTROY =
+        GameRuleRegistry.register("doCreepersExplosionDestroy", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
+
 
     @Override
     public void onInitialize() {
-
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            if(server.getWorld(RegistryKey.of(RegistryKeys.WORLD, new Identifier("dimension"))) != null){
+                world = server.getWorld(RegistryKey.of(RegistryKeys.WORLD, new Identifier("dimension")));
+            }
+        });
     }
 }
--- src/main/java/io/github/plainsvillager/anticreeper/mixin/CreeperEntityMixin.java
+++ src/main/java/io/github/plainsvillager/anticreeper/mixin/CreeperEntityMixin.java
@@ -1,9 +1,11 @@
 package io.github.plainsvillager.anticreeper.mixin;
 
+import io.github.plainsvillager.anticreeper.AntiCreeper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,10 +13,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static io.github.plainsvillager.anticreeper.AntiCreeper.DO_CREEPERS_EXPLOSION_DESTROY;
+
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin extends LivingEntity {
 
-    @Shadow private int fuseTime;
+    @Shadow
+    private int fuseTime;
+
+    private Explosion gameRule;
 
     public CreeperEntityMixin(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
@@ -23,24 +30,30 @@ public abstract class CreeperEntityMixin extends LivingEntity {
 
     /**
      * @author PlainsVillager
-     * @reason L
+     * @reason
      */
     @Overwrite
     public void explode() {
         if (!this.world.isClient) {
             this.dead = true;
-            this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0F, World.ExplosionSourceType.NONE);
+            if (!this.world.getGameRules().getBoolean(DO_CREEPERS_EXPLOSION_DESTROY)) {
+                this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0F, World.ExplosionSourceType.NONE);
+            } else {
+                this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0F, World.ExplosionSourceType.TNT); // here ain't any wrong!
+            }
             this.discard();
         }
     }
 
     /**
      * this make the pre explosion time became longer
-     * @author PlainsVillager
+     *
      * @param ci
+     * @author PlainsVillager
      */
     @Inject(at = @At("TAIL"), method = "<init>")
-    public void injectTo(CallbackInfo ci){
-        fuseTime = 300;
+    public void injectTo(CallbackInfo ci) {
+        fuseTime = 60;
     }
+
 }
