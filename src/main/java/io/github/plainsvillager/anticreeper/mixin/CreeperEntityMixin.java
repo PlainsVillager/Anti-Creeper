02f407b67f683d8c8d2459f1bdb7fc704d77897b
PlainsVillager <littlecuckoo@outlook.com>
2023-02-05 14:07:12 +0800

    1.3：苦力怕爆炸所需时长增至300tick

--- src/main/java/io/github/plainsvillager/anticreeper/mixin/CreeperEntityMixin.java
+++ src/main/java/io/github/plainsvillager/anticreeper/mixin/CreeperEntityMixin.java
@@ -6,9 +6,15 @@ import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin extends LivingEntity {
+
+    @Shadow private int fuseTime;
 
     public CreeperEntityMixin(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
@@ -26,5 +32,15 @@ public abstract class CreeperEntityMixin extends LivingEntity {
             this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0F, World.ExplosionSourceType.NONE);
             this.discard();
         }
+    }
+
+    /**
+     * this make the pre explosion time became longer
+     * @author PlainsVillager
+     * @param ci
+     */
+    @Inject(at = @At("TAIL"), method = "<init>")
+    public void injectTo(CallbackInfo ci){
+        fuseTime = 300;
     }
 }
